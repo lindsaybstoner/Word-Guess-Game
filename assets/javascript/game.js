15a7a93ccc2bf3ce2462cdbd80c0def62043076d
Blank <ellenmacy@Lindsays-MacBook-Pro.local>
2019-02-25 18:17:47 -0500

    last functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,6 +9,7 @@ var splitLetters = [];
 var rightLetters = [];
 var wrongLetters = [];
 var underscore = [];
+var correctLetters = [];
 
 // Create variables that hold references to the places in the HTML where we want to display things.
 var directionsText = document.getElementById("directions-text");
@@ -19,7 +20,7 @@ var letterGuessedText = document.getElementById("letterGuessed");
 var underscoreText = document.getElementById("underscore");
 
 //this function picks the word from the array
-pickCurrentWord = function() {
+pickCurrentWord = function () {
     currentWord = words[Math.floor(Math.random() * words.length)];
 }
 
@@ -27,7 +28,7 @@ pickCurrentWord();
 console.log(currentWord);
 
 //get current word length and then add 5 for how many wrong guesses the user gets
-numberOfGuess = function() {
+numberOfGuess = function () {
     numberOfGuessLeft = currentWord.length + 5;
 }
 
@@ -49,7 +50,7 @@ underscoreText.textContent = underscore;
 //underscore = underscore.split(" ");
 
 //get current word then split into array 
-splitCurrentWord = function() {
+splitCurrentWord = function () {
     splitLetters = currentWord.split("");
 }
 
@@ -67,46 +68,101 @@ function matchingLetters(letter) {
 
 
 //use index to loop through right letters and if letter guessed is the same get use the same index to replace
-function replaceUnderlines () {
+function replaceUnderlines() {
 
     for (var i = 0; i < rightLetters.length; i++) {
         if (rightLetters[i] == letterGuessed) {
             underscore.splice(i, 1, letterGuessed);
         }
-  
-    } 
-    if (!rightLetters.includes(letterGuessed)){
+        correctLetters = underscore.slice();
+    }
+
+    if (!rightLetters.includes(letterGuessed)) {
         numberOfGuessLeft--;
         //print *just* changed amount of guesses after wrong letter
-        numberOfGuessText.textContent = "Number of Guesses Left: " + numberOfGuessLeft; 
+        numberOfGuessText.textContent = "Number of Guesses Left: " + numberOfGuessLeft;
         letterGuessedText.textContent = "Letters Already Guessed: " + storeWrongLetters();
     }
 
 }
 
 //function to add all the wrong letters together to display more than just the most recent wrong letter
-function storeWrongLetters () {
+function storeWrongLetters() {
     wrongLetters.push(letterGuessed);
     return wrongLetters;
 }
 
+/* function finshedWord (underscore) {
+    return underscore == splitLetters;
+    
+} */
+
+//console.log(underscore.every(finshedWord));
+
+function finshedWord() {
+
+    if (correctLetters.toString() === splitLetters.toString() && underscore.toString() === splitLetters.toString()) {
+        wins = wins + 1;
+        console.log("wins");
+        resetGame();
+    }
+
+    else if (numberOfGuessLeft === 0) {
+        resetGame();
+    }
+
+}
+
+
+
+//reset game 
+function resetGame() {
+    
+    numberOfGuessLeft = 0;
+    currentWord = "";
+    letterGuessed = "";
+    words = ["rockies", "skiing", "mountains", "hiking", "biking", "sunny", "west", "microbrewiers", "outdoors"];
+    splitLetters = [];
+    rightLetters = [];
+    wrongLetters = [];
+    underscore = [];
+    correctLetters = [];
+
+    pickCurrentWord();
+
+    for (var i = 0; i < currentWord.length; i++) {
+        underscore.push(" __ ");
+        console.log(underscore);
+    }
+
+    
+    numberOfGuess();
+    splitCurrentWord();
+    console.log("reset");
+}
+
+
 // This function is run whenever the user presses a key.
-document.onkeyup = function(event) {
+document.onkeydown = function (event) {
 
 
-  // Determines which key was pressed.
-  letterGuessed = event.key;
+    // Determines which key was pressed.
+    letterGuessed = event.key;
 
     console.log(letterGuessed);
 
-    //only fun this function once a key was pressed
+    //only run this function once a key was pressed
     replaceUnderlines();
-        
-  
-    console.log(rightLetters);
-  
 
-   //print direction text even when keys have been pressed
+    finshedWord();
+    //checkFullWord();
+    console.log(finshedWord);
+
+    console.log(rightLetters);
+
+    
+
+    //print direction text even when keys have been pressed
     directionsText.textContent = "Press any key to get started!";
 
 
@@ -114,7 +170,10 @@ document.onkeyup = function(event) {
     winsText.textContent = "Wins: " + wins;
     underscoreText.textContent = underscore;
 
+    //checkFullWord();
+    //finshedWord();
+};
 
-  };
+
 
 
